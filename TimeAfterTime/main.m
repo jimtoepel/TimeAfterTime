5d689e23448f1c668b30560dce178a4ab630167e
jimtoepel <jim.toepel@gmail.com>
2015-02-13 19:56:19 -0800

    First start with Objective C

---
+++ TimeAfterTime/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  TimeAfterTime
+//
+//  Created by Jim Toepel on 2/13/15.
+//  Copyright (c) 2015 FunderDevelopment. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+
+        NSDate *now = [NSDate date];
+        NSLog(@"\nThis NSDate object lives at %p", now);
+        NSLog(@"\nHello, World!");
+    }
+    return 0;
+}
